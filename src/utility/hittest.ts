f97c0540e5a4312c37456a8f5a8f9af199d1afc0
Daniel Vogel <dvogel@uwaterloo.ca>
2024-09-20 17:05:42 -0400

    remove unused hittesting code

--- src/utility/hittest.ts
+++ src/utility/hittest.ts
@@ -1,5 +1,3 @@
-import { distance, closestPoint, Point2, point } from ".";
-
 export function insideHitTestRectangle(
   mx: number,
   my: number,
@@ -29,41 +27,4 @@ export function edgeHitTestRectangle(
   const inner =
     mx > x + s && mx < x + w - s && my > y + s && my < y + h - s;
   return outer && !inner;
-}
-
-export function hitTestLine(
-  mx: number,
-  my: number,
-  p0x: number,
-  p0y: number,
-  p1x: number,
-  p1y: number,
-  strokeWidth: number
-) {
-  // edge hit-test
-  const m = point(mx, my);
-  const q = closestPoint(m, point(p0x, p0y), point(p1x, p1y));
-  const d = distance(m.x, m.y, q.x, q.y);
-  if (d <= strokeWidth / 2) return true;
-
-  // no hit
-  return false;
-}
-
-export function edgeHitTestPolygon(
-  mx: number,
-  my: number,
-  points: Point2[],
-  strokeWidth: number
-) {
-  const m = new Point2(mx, my);
-  // assume shape is closed, so start with segment
-  // from last point to first point
-  let [p0] = points; // extract first point
-  for (let p1 of points) {
-    if (hitTestLine(m.x, m.y, p0.x, p0.y, p1.x, p1.y, strokeWidth))
-      return true;
-    p0 = p1;
-  }
-  return false;
 }
--- src/utility/index.ts
+++ src/utility/index.ts
@@ -1,5 +1,4 @@
 // export { BoundingBox } from "./boundingbox";
-export { closestPoint } from "./closestpoint";
 export { measureText } from "./measuretext";
 export * from "./misc";
 export * from "./vector2";
