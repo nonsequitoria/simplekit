950587658773f8e420108382c850955beacee4e5
Daniel Vogel <dvogel@uwaterloo.ca>
2024-09-23 12:27:44 -0400

    fix comments

--- src/utility/hittest.ts
+++ src/utility/hittest.ts
@@ -20,10 +20,10 @@ export function edgeHitTestRectangle(
 ) {
   // width of stroke on either side of edges
   const s = strokeWidth / 2;
-  // inside rect after adding stroke
+  // outside rect after adding stroke
   const outer =
     mx >= x - s && mx <= x + w + s && my >= y - s && my <= y + h + s;
-  // but NOT inside inner rect after subtracting stroke
+  // but NOT inside rect after subtracting stroke
   const inner =
     mx > x + s && mx < x + w - s && my > y + s && my < y + h - s;
   return outer && !inner;
