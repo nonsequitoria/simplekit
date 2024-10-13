45641b859c97af2e853f79abc9e29a886f1020a2
Daniel Vogel <dvogel@uwaterloo.ca>
2024-10-13 12:02:22 -0400

    nice comments

--- src/utility/hittest.ts
+++ src/utility/hittest.ts
@@ -1,3 +1,14 @@
+/**
+ * Checks if a point (mx, my) is inside a rectangle defined by its top-left corner (x, y) and dimensions (w, h).
+ *
+ * @param mx - The x-coordinate of the point to test.
+ * @param my - The y-coordinate of the point to test.
+ * @param x - The x-coordinate of the top-left corner of the rectangle.
+ * @param y - The y-coordinate of the top-left corner of the rectangle.
+ * @param w - The width of the rectangle.
+ * @param h - The height of the rectangle.
+ * @returns true if the point is inside the rectangle, false otherwise.
+ */
 export function insideHitTestRectangle(
   mx: number,
   my: number,
@@ -9,6 +20,19 @@ export function insideHitTestRectangle(
   return mx >= x && mx <= x + w && my >= y && my <= y + h;
 }
 
+/**
+ * Performs a hit test to determine if a point (mx, my) is on the edge of a rectangle.
+ * The edge is defined by the stroke width around the rectangle.
+ *
+ * @param mx - The x-coordinate of the point to test.
+ * @param my - The y-coordinate of the point to test.
+ * @param x - The x-coordinate of the top-left corner of the rectangle.
+ * @param y - The y-coordinate of the top-left corner of the rectangle.
+ * @param w - The width of the rectangle.
+ * @param h - The height of the rectangle.
+ * @param strokeWidth - The width of the stroke around the rectangle.
+ * @returns true if the point is on the edge of the rectangle, false otherwise.
+ */
 export function edgeHitTestRectangle(
   mx: number,
   my: number,
